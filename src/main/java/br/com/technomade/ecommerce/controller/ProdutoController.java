138f4630ec3b1c9ed9380ce71f962c9a9f33d7a7
Bruno Vinicius Fedora <brunohubbe@gmail.com>
2026-01-26 09:32:46 -0300

    feat: adicionando logica de ativar e get com filtro

--- src/main/java/br/com/technomade/ecommerce/controller/ProdutoController.java
+++ src/main/java/br/com/technomade/ecommerce/controller/ProdutoController.java
@@ -1,5 +1,8 @@
 package br.com.technomade.ecommerce.controller;
 
+import br.com.technomade.ecommerce.dto.produto.AtivarProdutoRequestDTO;
+import br.com.technomade.ecommerce.dto.produto.InativarProdutoRequestDTO;
+import br.com.technomade.ecommerce.dto.produto.ProdutoUpdateDTO;
 import br.com.technomade.ecommerce.model.Produto;
 import br.com.technomade.ecommerce.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +19,32 @@ public class ProdutoController {
     private ProdutoService produtoService;
 
     @GetMapping
-    public List<Produto> listar() {
-        return produtoService.listarTodos();
+    public List<Produto> listar(
+            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) String descricao,
+            @RequestParam(required = false) String marca,
+            @RequestParam(required = false) String codigoBarras,
+            @RequestParam(required = false) Long categoriaId,
+            @RequestParam(required = false) Long grupoPrecificacaoId,
+            @RequestParam(required = false) Boolean ativo,
+            @RequestParam(required = false) Double valorVendaMin,
+            @RequestParam(required = false) Double valorVendaMax,
+            @RequestParam(required = false) Double valorCustoMin,
+            @RequestParam(required = false) Double valorCustoMax
+    ) {
+        return produtoService.listarComFiltros(
+                nome,
+                descricao,
+                marca,
+                codigoBarras,
+                categoriaId,
+                grupoPrecificacaoId,
+                ativo,
+                valorVendaMin,
+                valorVendaMax,
+                valorCustoMin,
+                valorCustoMax
+        );
     }
 
     @GetMapping("/{id}")
@@ -32,8 +59,23 @@ public class ProdutoController {
         return produtoService.salvar(produto);
     }
 
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, @RequestBody ProdutoUpdateDTO dto){
+        return produtoService.atualizar(id, dto);
+    }
+
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id) {
         produtoService.deletarPorId(id);
+    }
+
+    @PutMapping("/{id}/inativar")
+    public Produto inativar(@PathVariable Long id, @RequestBody InativarProdutoRequestDTO dto){
+        return produtoService.inativar(id, dto.getMotivo(), dto.getJustificativa());
+    }
+
+    @PutMapping("/{id}/ativar")
+    public Produto ativar(@PathVariable Long id, @RequestBody AtivarProdutoRequestDTO dto){
+        return produtoService.ativar(id, dto.getMotivo(), dto.getJustificativa());
     }
 }
