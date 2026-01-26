07ae07ed419f28a0caa4512a0b0b1bb3fe8d9eac
Bruno Vinicius Fedora <brunohubbe@gmail.com>
2026-01-26 09:39:33 -0300

    feat: enum de motivo ativacao/desativacao

---
+++ src/main/java/br/com/technomade/ecommerce/model/MotivoAtivacao.java
@@ -0,0 +1,9 @@
+package br.com.technomade.ecommerce.model;
+
+public enum MotivoAtivacao {
+    LANCAMENTO,
+    RETORNO_MERCADO,
+    VOLTOU_AO_ESTOQUE,
+    CORRECAO_CADASTRO,
+    OUTRO
+}
---
+++ src/main/java/br/com/technomade/ecommerce/model/MotivoInativacao.java
@@ -0,0 +1,8 @@
+package br.com.technomade.ecommerce.model;
+
+public enum MotivoInativacao {
+    FORA_DE_MERCADO,
+    SEM_ESTOQUE,
+    DESCONTINUADO,
+    OUTRO
+}
