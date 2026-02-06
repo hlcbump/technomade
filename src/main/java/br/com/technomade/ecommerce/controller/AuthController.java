14677ee9eddfdb8cddaef008ecb186b8054320ed
Bruno Vinicius Fedora <brunohubbe@gmail.com>
2026-02-06 14:06:50 -0300

    fix: alterando para apenas a porta 8000

--- src/main/java/br/com/technomade/ecommerce/controller/AuthController.java
+++ src/main/java/br/com/technomade/ecommerce/controller/AuthController.java
@@ -9,13 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = {"http://localhost:8000"})
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
