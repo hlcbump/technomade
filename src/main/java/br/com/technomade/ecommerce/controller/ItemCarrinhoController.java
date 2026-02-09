52bfc8cf2d9da7b507b0c838b8815eb55fe5f2a4
Bruno Vinicius Fedora <brunohubbe@gmail.com>
2026-02-09 08:51:35 -0300

    feat: add métodos relacionados a expiração de itens no carrinho

--- src/main/java/br/com/technomade/ecommerce/controller/ItemCarrinhoController.java
+++ src/main/java/br/com/technomade/ecommerce/controller/ItemCarrinhoController.java
@@ -5,10 +5,13 @@ import br.com.technomade.ecommerce.dto.carrinho.ItemCarrinhoResponseDTO;
 import br.com.technomade.ecommerce.dto.carrinho.ItemCarrinhoUpdateDTO;
 import br.com.technomade.ecommerce.service.ItemCarrinhoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:8000"})
 @RestController
 @RequestMapping("/api/carrinho")
 public class ItemCarrinhoController {
@@ -34,5 +37,20 @@ public class ItemCarrinhoController {
     @DeleteMapping("/{id}")
     public void remover(@PathVariable Long id){
         itemCarrinhoService.remover(id);
+    }
+
+    // retorna minutos restantes atee expiração do carrinho
+    @GetMapping("/tempo-restante")
+    public ResponseEntity<Map<String, Long>> calcularTempoRestante() {
+        long minutos = itemCarrinhoService.calcularMinutosRestantes();
+        return ResponseEntity.ok(Map.of("minutosRestantes", minutos));
+    }
+
+    // notifica usuario se carrinho está prestes a expirar / front chama
+    @PostMapping("/verificar-expiracao")
+    public ResponseEntity<Map<String, Boolean>> verificarExpiracao() {
+        itemCarrinhoService.notificarSeProximoExpiracao();
+        boolean deveNotificar = itemCarrinhoService.deveNotificarExpiracao();
+        return ResponseEntity.ok(Map.of("proximoExpiracao", deveNotificar));
     }
 }
